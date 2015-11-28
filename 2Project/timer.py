e55860451bde55de9154808b9856f312d2904733
Anthony Dario <anthonyrdario@gmail.com>
2015-11-28 13:21:53 -0800

    Doesn't timeout anymore
    Gets info from one packet

--- 2Project/timer.py
+++ 2Project/timer.py
@@ -1,5 +1,7 @@
 import time
+import select
 import socket
+import errno
 from struct import *
 
 # file stuff
@@ -10,7 +12,8 @@ for addr in addresses:
     # some useful variables
     icmp = socket.getprotobyname('icmp')
     udp = socket.getprotobyname('udp')
-    ttl = 255
+    ttl = 32
+    timeout = 3
     addr = addr.rstrip()
 
     # we will listen on port 33434, like traceroute
@@ -19,6 +22,7 @@ for addr in addresses:
     # create our sockets
     recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
     send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+    recv_socket.setblocking(0)
 
     # change the fields
     send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
@@ -26,24 +30,36 @@ for addr in addresses:
     # bind the socket
     recv_socket.bind(("", port))
 
-    # get the data
+    # for the select call
+    incoming = [ recv_socket ]
+    outgoing = [] 
+    potential_errors = {}
+
     start = time.time()
     send_socket.sendto("", (addr, port))
-    data , address  = recv_socket.recvfrom(512)
-    end = time.time()
 
-    # parse the data first 20 characters are the ip header
-    ip_header = data[0:20]
-    iph = unpack('BBHHHBBH4s4s', ip_header)
-
-    packet_ttl = iph[5]
-
-    # we have the TTL from the source, how to figure out actual ttl...
+    readable, writable, errors = \
+        select.select(incoming, outgoing, potential_errors, timeout)
+    
     try:
-        hostname = socket.gethostbyaddr(address[0])[0]
-    except socket.error:
-        hostname = 'unknown'
-    print hostname 
-    print '\tTTL: ' + str(packet_ttl)
-    print '\tTime: ' + str(end - start)
+        data, address = recv_socket.recvfrom(1024)
+        end = time.time()
+    except socket.error, e:
+        print 'could not get data from socket: ' + addr
+    else:
+
+        # parse the data first 20 characters are the ip header
+        ip_header = data[0:20]
+        iph = unpack('BBHHHBBH4s4s', ip_header)
+
+        packet_ttl = iph[5]
+
+        # we have the TTL from the source, how to figure out actual ttl...
+        try:
+            hostname = socket.gethostbyaddr(address[0])[0]
+        except socket.error:
+            hostname = 'unknown'
+        print address[0] + ' : ' + hostname 
+        print '\tTTL: ' + str(packet_ttl)
+        print '\tTime: ' + str(end - start)
 
