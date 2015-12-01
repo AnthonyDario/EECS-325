f06a257e6a0d2ae5b2fd73058cf56db51dba3b42
Anthony Dario <anthonyrdario@gmail.com>
2015-12-01 16:48:45 -0500

    Added an ID to the packet field

--- 2Project/timer.py
+++ 2Project/timer.py
@@ -4,11 +4,12 @@ import socket
 import errno
 from struct import *
 
-id = 0
+id_num = 0
 
 def get_time(addr, times_checked):
 
-    global id
+    global id_num
+    id_num += 1
     icmp = socket.getprotobyname('icmp')
     udp = socket.getprotobyname('udp')
     ttl = 32
@@ -26,7 +27,7 @@ def get_time(addr, times_checked):
     send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
     # bind the socket
-    recv_socket.bind((str(id), port))
+    recv_socket.bind(("", port))
 
     # for the select call
     incoming = [ recv_socket ]
@@ -39,14 +40,13 @@ def get_time(addr, times_checked):
         select.select(incoming, outgoing, potential_errors, timeout)
 
     send_socket.sendto("", (addr, port))
-    print 'sent to ' + addr
+
+    print readable
 
     if len(readable) == 0:
         # we couldn't get any data try rechecking
         print 'could not get data from socket: ' + addr +\
               ' on attempt ' + str(3 - times_checked)
-        if times_checked > 0:
-            get_time(addr, times_checked - 1)
 
     else:
         data, address = recv_socket.recvfrom(1024)
@@ -55,36 +55,44 @@ def get_time(addr, times_checked):
         # parse the data first 20 characters are the ip header
         icmp_header = data[20:28]
         our_ip_header = data[28:48]
+        contents = data[56:58]
+
         iph = unpack('BBHHHBBH4s4s', our_ip_header)
         icmph = unpack('bbHHh', icmp_header)
+        response_id = unpack(str(len(contents)) + 's', contents)
 
         packet_ttl = iph[5] 
         icmp_type, icmp_code, _, _, _ = icmph
 
-        # we have the TTL from the source, how to figure out actual ttl...
         try:
             hostname = socket.gethostbyaddr(address[0])[0]
         except socket.error:
             hostname = 'unknown'
+
         print address[0] + ' : ' + hostname 
         print '\tcode: ' + str(icmp_code)
         print '\ttype: ' + str(icmp_type)
         print '\tnumber of hops: ' + str(ttl - packet_ttl)
         print '\tTTL: ' + str(packet_ttl)
         print '\tTime: ' + str((end - start) * 1000)
+        print '\tID: ' + str(response_id)
+        print '\tour ID: ' + str(id_num)
 
-        readable, writable, errors = \
-            select.select(incoming, outgoing, potential_errors, timeout)
-
-        # in case an address sends back multiple packets for whatever reason
-        while len(readable) == 1:
-            recv_socket.recvfrom(1024)
-            readable, writable, errors = \
-                select.select(incoming, outgoing, potential_errors, timeout)
+        #readable, writable, errors = \
+        #    select.select(incoming, outgoing, potential_errors, timeout)
+#
+#        # in case an address sends back multiple packets for whatever reason
+#        while len(readable) == 1:
+#            data, address = recv_socket.recvfrom(1024)
+#            contents = data[56:58]
+#            print unpack(str(len(contents)) + 's', contents)
+#            readable, writable, errors = \
+#                select.select(incoming, outgoing, potential_errors, timeout)
 
     send_socket.close()
     recv_socket.close()
 
+# error with sending multiple
 # match
 # graphs
 # write a report
