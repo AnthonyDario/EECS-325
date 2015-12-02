f3ab13e930c7d2d2f8dce8f4cd9870ddb210abd8
Anthony Dario <anthonyrdario@gmail.com>
2015-12-01 21:28:19 -0500

    Added current location

--- 2Project/geoDistance.py
+++ 2Project/geoDistance.py
@@ -1,9 +1,22 @@
 import urllib2
+import json
 
 addresses = open('targets.txt', 'r')
 
+rad_earth = 6371000
+
+location = json.loads(urllib2.urlopen("http://freegeoip.net/json/").read())
+lat = location['latitude']
+lon = location['longitude']
+
+print lat
+print lon
+
 for address in addresses:
     address.rstrip()
-    content = urllib2.urlopen("http://freegeoip.net/json/" + address).read()
-    print content
+    content = (urllib2.urlopen("http://freegeoip.net/json/" + address).read())
+    data = json.loads(content)
+    print data['ip']
+    print '\tlatitude: ' + str(data['latitude'])
+    print '\tlongitude: ' + str(data['longitude'])
 
